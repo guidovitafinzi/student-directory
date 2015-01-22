1f08ee010e4e3f2026ed83730f10faad58a5420b
Guido Vita Finzi <guido.vitafinzi@gmail.com>
2015-01-22 18:33:42 +0100

    Added a list of students

---
+++ directory.rb
@@ -0,0 +1,31 @@
+puts "The students of my cohort at Makers Academy"
+puts "------------------"
+
+puts "Christopher Ward"
+puts "Rizwan Ali" 
+puts "Vanessa Virgitti" 
+puts "Tommaso Bratto" 
+puts "Joe Newman" 
+puts "Loris Fochesato" 
+puts "Guido Vita Finzi" 
+puts "Tom Coakes"
+puts "Kevin Lanzon" 
+puts "Sebastien Pires" 
+puts "Yannick Brunner" 
+puts "Costas Sarris" 
+puts "Diego Romero" 
+puts "Erik Griffn" 
+puts "Thomas Strothjohann" 
+puts "Alex Parkinson" 
+puts "Carrie Christenson" 
+puts "Julian Veling" 
+puts "Edward Brown" 
+puts "Meads Chalcroft" 
+puts "Sean Haughton" 
+puts "Paul Fitzgerald" 
+puts "Phil Brockwell" 
+puts "Kate Beavis" 
+
+
+
+
