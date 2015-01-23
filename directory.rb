afe69c00e1ed33ad518f42856f7bb15146a23e7f
Guido Vita Finzi <guido.vitafinzi@gmail.com>
2015-01-23 14:00:06 +0100

    Added methods
    Added print_header method.
    Added print method to iterate through students array.
    Added print_footer.

--- directory.rb
+++ directory.rb
@@ -26,14 +26,28 @@ students = [
   "Kate Beavis"
 ]
 
-#and print them
-puts "The students of my cohort at Makers Academy"
-puts "------------------------"
-students.each do |student|
-	puts student
+def print_header
+	puts "The students of my cohort at Makers Acaemy"
+	puts "------------------"
 end
 
-#finally, we print the total
-puts "Overall, we have #{students.length} great students"
+def print(names)
+	names.each do |name|
+		puts name
+	end
+end
+
+def print_footer(names)
+	puts "Overall, we have #{names.length} great students"
+end
+
+#calling the methods
+print_header
+print(students)
+print_footer(students)
+
+
+
+
 
 
