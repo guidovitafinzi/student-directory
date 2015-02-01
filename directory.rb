e2f5afd40040b8b713478c330c248ce36db5af38
Guido Vita Finzi <guido.vitafinzi@gmail.com>
2015-02-01 19:39:54 +0000

    Added save_students
    save_students method will save students data in students.csv
    added .gitignore to ignore students.csv

--- directory.rb
+++ directory.rb
@@ -21,14 +21,12 @@ end
 def input_students
 	puts "Please enter the names of the students"
 	puts "To finish, just hit return twice"
-	# create an empty array
-	@students = []
 	# get the first name
 	name = gets.chomp
 	#while the name is not empty, repeat this code
 	while !name.empty? do
 		#add the student hash to the array
-		@students << {:name => name, :cohort => :february}
+		@students << {:name => name, :cohort => :February}
 		puts "Now we have #{@students.length} students"
 		#get another name from the user
 		name = gets.chomp
@@ -40,6 +38,7 @@ end
 def print_menu
 	puts "1. Input the students"
 	puts "2. Show the students"
+	puts "3. Save the list to students.csv"
 	puts "9. Exit"
 end
 
@@ -49,12 +48,26 @@ def show_students
 	print_footer
 end
 
+def save_students
+	# Open the file for writing
+	file = File.open("students.csv", "w")
+	# iterate over the array of students
+	@students.each do |student|
+		student_data = [student[:name], student[:cohort]]
+		csv_line = student_data.join(",")
+		file.puts csv_line
+	end
+	file.close
+end
+
 def process(selection)
 	case selection
 		when "1"
 		input_students
 		when "2"
 		show_students
+		when "3"
+		save_students
 		when "9"
 		exit
 		else
